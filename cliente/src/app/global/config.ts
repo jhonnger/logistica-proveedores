2f12c3d27b5b1f5d5260a70b6939035aa712473f
Johnny Rogger Cotos Sullon <jhonny_94_17@hotmail.com>
2019-04-21 10:49:49 -0500

    fix endpoint

--- cliente/src/app/global/config.ts
+++ cliente/src/app/global/config.ts
@@ -1,7 +1,7 @@
 
-export const URL_SERVER = location.protocol + '//' + location.hostname + ':5555/logistica/';
-// export const URL_SERVER = location.protocol+'//'+location.hostname+':'+location.port+'/logistica/';
+// export const URL_SERVER = location.protocol + '//' + location.hostname + ':5555/logistica/';
+ export const URL_SERVER = location.protocol + '//' + location.hostname + ':' + location.port + '/logistica/';
 
 
-export const URL_SERVER_SOCKET = location.protocol + '//' + location.hostname + ':5555/logistica/';
-// export const URL_SERVER_SOCKET = location.protocol+'//'+location.hostname+':'+location.port+'/';
+// export const URL_SERVER_SOCKET = location.protocol + '//' + location.hostname + ':5555/logistica/';
+ export const URL_SERVER_SOCKET = location.protocol + '//' + location.hostname + ':' + location.port + '/';
